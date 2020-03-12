40fb910132dc412eee5d46fa7aed7e7f63d6ee09
帅乐乐 <44996764+shuailele@users.noreply.github.com>
2020-03-12 06:51:29 +0000

    Update Ant.java
    增加了车型选择功能

--- ServiceCompetition_ANT/src/Ant.java
+++ ServiceCompetition_ANT/src/Ant.java
@@ -3,16 +3,13 @@
 //当输入最大里程数小于全部距离时  程序会无响应  死循环
 
 import java.util.ArrayDeque;
-import java.util.LinkedList;
 import java.util.Random;
-import java.util.Vector;
 
 public class Ant {                  //蚂蚁类
 
     private Random random;   //用于获取随机数
     private double capacity;        //车的承载量
     private double room;            //车辆空余的空间
-    //private boolean pass[];         //车辆回配送中心允许经过的点     false表示未经过 true表示经过
     private static final double maxRemoving = Main.maxRemoving;     //车辆行驶的最大公里数
     private boolean allowChoseNextCity = true;      //是否允许访问下一个城市  true为允许  false为不允许
     private boolean destroy = false;                //当前路径是否恶意增加信息素
@@ -25,6 +22,7 @@ public class Ant {                  //蚂蚁类
     public double length;           //路径长度
     public ArrayDeque<Integer> tabu;     //配送顺序     包括了返程的路程
     public ArrayDeque<Double> remainingRoom;           //回到配送中心时车辆的满载率
+    public int used = 0;                    //表示车辆被使用过的次数
 
     //默认构造函数
     public Ant(){
@@ -39,14 +37,10 @@ public class Ant {                  //蚂蚁类
         random = new Random(System.currentTimeMillis());        //使用系统时间作为种子
         this.capacity = capacity;
         room = capacity;
-        //pass = new boolean[Map.clientCount + 1];
         path = new LinkedHashSet<>();
         length = 0;
         tabu = new ArrayDeque<>();
         remainingRoom = new ArrayDeque<>();
-
-        /*for(int i = 0 ; i < Map.clientCount + 1 ; ++i)
-            pass[i] = false;*/
     }
 
     //设置是否允许访问下一个城市
@@ -80,20 +74,23 @@ public class Ant {                  //蚂蚁类
 
         tabu.clear();
 
+        used = 0;
+
         remainingRoom.clear();
 
         path.clear();
-        addPlace(0);
     }
 
     //回到配送中心后的信息重置
     public void resetGo(){
-        /*for(int i = 0 ; i < Map.clientCount + 1 ; ++i)
-            pass[i] = false;*/
-
         allowChoseNextCity = true;
 
         room = capacity;
+
+        path.clear();
+        tabu.clear();
+        addPlace(0);
+        length = 0;
     }
 
     //把地点place添加到已走过的数组中
@@ -108,16 +105,14 @@ public class Ant {                  //蚂蚁类
             length += Map.distance[tabu.getLast()][place];      //更新路径长度
         }
 
-        if(judgeDestroy(place))       //如果恶意破坏那么 将表示位置为true
+        if(judgeDestroy(place)){
+            //如果恶意破坏那么 将表示位置为true
             destroy = true;
-//        if(place == 0){
-//            path.clear();
-//            path.addLast(0);
-//        }
+        }
 
         tabu.add(place);
 
-        if(/*place != 0 && */room >= Map.cc[place].need && Map.ok[place] == false){
+        if(room >= Map.cc[place].need && Map.ok[place] == false){
             if(Map.cc[place].need == 0 && place != 0)
                 return pathLength;
             Map.ok[place] = true;
@@ -134,100 +129,9 @@ public class Ant {                  //蚂蚁类
 
         double p;       //下面这行根据两点间的距离越大且地图上的信息素浓度越大蚂蚁更渴望走这条路产生的信息素越多
         p = (alpha / (alpha + beta) * Map.pheromones[from][to]) + (beta / (alpha + beta) * (1 / Map.distance[from][to]));
-//        double p=Math.pow((1.0/Map.distance[from][to]),beta)*Math.pow((Map.distance[from][to]),alpha);	//转移期望
-//        if( p<=0)p=rnd(0,1)*Math.pow( (1.0 / Map.distance[from][to]), beta);  //如果没有其他蚂蚁走过，就按距离选择
 
         return p;
     }
-
-    //选择下一个地点
-//    public int chooseNextPlace(){
-//        int to = -1;                                //to为下一个要到的城市
-//        int curPlace = tabu.getLast();              //当前城市
-//        int i;
-//        double hormone=0;	//信息总量
-//
-//        if(AntProject.count < 6){
-//
-//            for(i = 1 ; i < Map.clientCount + 1 ; ++i)
-//                if(Map.cc[i].need != 0 && Map.distance[curPlace][i] > 0 && Map.ok[i] == false &&
-//                        Map.cc[i].need <= room && Map.distance[curPlace][i] <= maxRemoving)
-//                    hormone += antProduct(curPlace, i);         //计算信息总量
-//
-////            if(hormone == 0.0 && Map.distance[curPlace][0] > 0 && (Map.cc[0].need >= room || Map.distance[curPlace][0] > maxRemoving))
-////                hormone += antProduct(curPlace, 0);
-//
-//            if(hormone == 0.0){           //如果所有顾客配送完成
-//                while(true){
-//                    if(Map.distance[curPlace][0] > 0){
-//                        to = 0;
-//                        break;
-//                    }
-//
-//                    int place = rnd(Map.clientCount);
-//
-//
-////                    ArrayDeque<Integer> temp = tabu.clone();
-////                    while (!temp.isEmpty()){
-////                        System.out.print(temp.removeFirst());
-////                        if(!temp.isEmpty())
-////                            System.out.print("->");
-////                    }
-////                    System.out.println("cur="+curPlace);
-////                    System.out.println("place="+place);
-////                    //System.out.println("pass="+pass[place]);
-////                    System.out.println("max.distance"+Map.distance[curPlace][place]);
-////                    System.out.println("map.ok"+Map.ok[place]);
-//
-//                    if(Map.distance[curPlace][place] > 0 /*&& pass[place] == false*/){
-//                        /*pass[curPlace] = true;
-//                        pass[place] = true;*/
-//                        to = place;
-//                        break;
-//                    }
-//                }
-//            }
-//            else{
-//                for(to = 1 ; to < Map.clientCount + 1 ; ++to){
-//                    if(Map.distance[curPlace][to] > 0 && Map.ok[to] == false &&
-//                            Map.cc[to].need <= room && Map.distance[curPlace][to] <= maxRemoving){
-//                        double p = antProduct(curPlace,to) / hormone;
-//                        if(rnd(0,1) < p)	    //按转移概率大小选择下一个城市，同时也避免了局部最优解
-//                            break;
-//                    }
-//                }
-//            }
-//
-//            if(to == Map.clientCount + 1){
-//                hormone = -1;
-//                for(i = 1 ; i < Map.clientCount + 1 ; ++i)
-//                    if(Map.distance[curPlace][i] > 0 && Map.ok[i] == false &&
-//                            Map.cc[i].need <= room && Map.distance[curPlace][i] <= maxRemoving)
-//                    {
-//                        if (hormone < antProduct(curPlace, i)) {
-//                            hormone = antProduct(curPlace,i);	//如果上一步选择失败，则选择具有最大信息量的城市
-//                            to = i;
-//                        }
-//                    }
-//            }
-//        }
-//        else{                   //当连续6代都没有产生更短的路径，则
-//            for ( i = 1 ; i < Map.clientCount + 1 ; ++i) {
-//                if(Map.distance[curPlace][i] > 0 && Map.ok[i] == false &&
-//                        Map.cc[i].need <= room && Map.distance[curPlace][i] <= maxRemoving) {
-//                    to = i;
-//                    AntProject.count = 0;
-//                    AntProject.rou = 0.5;
-//
-//                    if(rnd(0.0,1.0) > 0.5)	//随机选择一个还没访问的城市
-//                    {
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//        return to;
-//    }
 
     //选择下一城市2
     private int chooseNextPlace(){
@@ -250,11 +154,6 @@ public class Ant {                  //蚂蚁类
             if(hormone == 0.0){                 //如果直接相连点全部无法配送
 
                 while(true){
-//                    if(Map.distance[curPlace][0] > 0){
-//                        to = 0;
-//                        break;
-//                    }
-
                     int place = rnd(Map.clientCount);
 
                     if(Map.distance[curPlace][place] > 0){
@@ -349,8 +248,6 @@ public class Ant {                  //蚂蚁类
                 return true;
 
             path.add(nextPlace);
-//            if(path.size() > 1 && path.peekFirst() == path.peekLast())
-//                return true;
         }
 
         return false;
@@ -406,6 +303,23 @@ public class Ant {                  //蚂蚁类
             }
         }
         return -1;
+    }
+
+    //复制信息给当前蚂蚁
+    public void copyInformation(Ant ant){
+        room = capacity - (ant.capacity - ant.room);
+
+        length += ant.length;
+
+        ArrayDeque<Integer> temp = ant.tabu.clone();
+        if(!tabu.isEmpty())
+            temp.removeFirst();
+
+        while (!temp.isEmpty()){
+            tabu.add(temp.removeFirst());
+        }
+
+        remainingRoom.add((capacity - room) / capacity);
     }
 
     //获得随机数, 范围为 [low, uper]
